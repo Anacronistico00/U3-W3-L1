e9419ee80b6043289f4f94b8b6121cab49925aa9
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-20 15:13:03 +0100

    Task completed succesfully / going to implement the Extra

---
+++ src/components/FavouritesList.jsx
@@ -0,0 +1,60 @@
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const FavouritesList = () => {
+  const jobs = useSelector((state) => {
+    return state.favourites.list;
+  });
+
+  const dispatch = useDispatch();
+
+  return (
+    <Container>
+      {jobs.map((job, i) => {
+        return (
+          <Row
+            key={i}
+            className='mx-0 mt-3 p-3'
+            style={{ border: '1px solid #00000033', borderRadius: 4 }}
+          >
+            <Col xs={3}>
+              <Link to={`/${job.company_name}`}>{job.company_name}</Link>
+            </Col>
+            <Col xs={6}>
+              <a href={job.url} target='_blank' rel='noreferrer'>
+                {job.title}
+              </a>
+            </Col>
+            <Col xs={3} className='text-center d-flex'>
+              <Button
+                className='m-2'
+                onClick={() => {
+                  dispatch({
+                    type: 'ADD_TO_FAVOURITES',
+                    payload: job,
+                  });
+                }}
+              >
+                Add to favorites
+              </Button>
+              <Button
+                className='m-2'
+                onClick={() => {
+                  dispatch({
+                    type: 'REMOVE_FROM_FAVOURITES',
+                    payload: job._id,
+                  });
+                }}
+              >
+                Remove from favorites
+              </Button>
+            </Col>
+          </Row>
+        );
+      })}
+    </Container>
+  );
+};
+
+export default FavouritesList;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,18 @@
 const initialState = {
-  favorites: {
+  favourites: {
     list: [],
   },
 };
 
 const mainReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'ADD_TO_FAVOURITES':
+      return {
+        ...state,
+        favourites: {
+          list: [...state.favourites.list, action.payload],
+        },
+      };
     default:
       return state;
   }
